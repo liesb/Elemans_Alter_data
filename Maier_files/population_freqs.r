cd3d61350a7b859aeac634113dcdd277f0f967c4
Lies <lies.boelen@gmail.com>
2017-01-30 16:47:05 +0000

    Update population_freqs.r

--- Maier_files/population_freqs.r
+++ Maier_files/population_freqs.r
@@ -1,5 +1,5 @@
 # Contribution of ethnic groups to the HIV+ population in the US
 # European-American, African-American, Asian/Pacific Islander, Hispanic
 
-ethnic.freq <- c(0.376,0.524,0.083,0.004)	#data 2000
+ethnic.freq <- c(0.376,0.524,0.004,0.083)	#data 2000
 names(ethnic.freq) <- c('EUR','AFA','API','HIS')
